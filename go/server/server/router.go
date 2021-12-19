06a4b06a08408999ad9d50176c38efd6bc32d06d
Nicolas Vincent <nicolas.vincent@grenoble-inp.org>
2021-12-19 21:46:33 +0100

    Refactor handler.go

---
+++ go/server/server/router.go
@@ -0,0 +1,39 @@
+package server
+
+import (
+	"html/template"
+
+	"github.com/ensimag-oprf/go/server/controllers"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func NewRouter() (*echo.Echo, error) {
+	router := echo.New()
+
+	// Template renderer
+	renderer := &Template{
+		templates: template.Must(template.ParseGlob("public/*.html")),
+	}
+	router.Renderer = renderer
+
+	// Middlewares
+	router.Use(middleware.Logger())
+	router.Use(middleware.Recover())
+	router.Use(middleware.Gzip())
+	router.Use(middleware.CORS())
+
+	// Endpoints
+	router.GET("/", controllers.IndexHandler)
+
+	oprfServerController := controllers.NewOPRFServerController()
+	oprfServerController.Initialize()
+
+	router.GET("/request_public_keys", oprfServerController.GetKeysHandler)
+	router.POST("/evaluate", oprfServerController.EvaluateHandler)
+
+	// Static files
+	router.Static("/static", "./static")
+
+	return router, nil
+}

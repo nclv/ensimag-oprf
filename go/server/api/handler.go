db45d809b9cff17aa7a3da47568aab27190059d4
Nicolas Vincent <nicolas.vincent@grenoble-inp.org>
2021-12-19 22:20:15 +0100

    Test handler

--- go/server/api/handler.go
+++ go/server/api/handler.go
@@ -1,21 +1,37 @@
 package api
 
 import (
-	"log"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"net/http"
-
-	"github.com/ensimag-oprf/go/server/server"
 )
 
-func Handler(w http.ResponseWriter, r *http.Request) {
-	log.Println("hello")
-	router, err := server.NewRouter()
-	if err != nil {
-		log.Println(err)
-
-		return
-	}
-	log.Println("router")
-
-	router.ServeHTTP(w, r)
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello World")
 }
+
+func hello2(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello World2")
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.GET("/api/", hello)
+	e.GET("/api/2", hello2)
+
+	e.ServeHTTP(w, r)
+}
+
+//
+//func Handler(w http.ResponseWriter, r *http.Request) {
+//	router, err := server.NewRouter()
+//	if err != nil {
+//		log.Println(err)
+//
+//		return
+//	}
+//
+//	router.ServeHTTP(w, r)
+//}

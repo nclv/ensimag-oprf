2542914a1d46c091705adc69b91c9418f778d915
Nicolas Vincent <nicolas.vincent@grenoble-inp.org>
2021-12-19 22:45:33 +0100

    Test handler

---
+++ go/server/api/requestPublicKeys.go
@@ -0,0 +1,18 @@
+package api
+
+import (
+	"github.com/ensimag-oprf/go/server/controllers"
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	router := echo.New()
+
+	oprfServerController := controllers.NewOPRFServerController()
+	oprfServerController.Initialize()
+
+	router.GET("/request_public_keys", oprfServerController.GetKeysHandler)
+
+	router.ServeHTTP(w, r)
+}

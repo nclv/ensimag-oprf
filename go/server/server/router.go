0a4ad7609c31506bb1a9fe9e1def7357c4c1bb1f
Nicolas Vincent <nicolas.vincent@grenoble-inp.org>
2021-12-19 22:06:12 +0100

    Update template URL

--- go/server/server/router.go
+++ go/server/server/router.go
@@ -1,28 +1,18 @@
 package server
 
 import (
-	"fmt"
-	"html/template"
-	"log"
-	"os"
-
 	"github.com/ensimag-oprf/go/server/controllers"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"html/template"
 )
 
 func NewRouter() (*echo.Echo, error) {
 	router := echo.New()
 
-	dir, err := os.Getwd()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(dir)
-
 	// Template renderer
 	renderer := &Template{
-		templates: template.Must(template.ParseGlob("public/*.html")),
+		templates: template.Must(template.ParseGlob("*.html")),
 	}
 	router.Renderer = renderer
 
